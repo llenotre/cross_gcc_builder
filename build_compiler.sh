62237d970b3b9a904c6b18c97b37a7b335656b2e
llenotre <llenotre@student.42.fr>
2021-04-05 20:22:41 +0100

    Compilation fix

--- build_compiler.sh
+++ build_compiler.sh
@@ -2,19 +2,18 @@
 
 export TARGET=i686-elf
 
-tar xvf binutils-2.36.tar.xz
-tar xvf gcc-10.2.0.tar.xz
-
-mkdir binutils_build/
-
-cd binutils_build
-../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
-make
-make install
+#mkdir binutils_build/
+#cd binutils_build
+#../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
+#make
+#make install
+#cd ..
 
 mkdir gcc_build/
-../gcc-10.2.0/configure --target=$TARGET --prefix=/usr --disable-nls --without-headers
+cd gcc_build
+../gcc-10.2.0/configure --target=$TARGET --prefix=/usr --disable-nls --enable-languages=c --without-headers
 make all-gcc
 make all-target-libgcc
 make install-gcc
 make install-target-libgcc
+cd ..
--- download.sh
+++ download.sh
@@ -2,3 +2,6 @@
 
 wget https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.36.tar.xz
+
+tar xvf binutils-2.36.tar.xz
+tar xvf gcc-10.2.0.tar.xz
