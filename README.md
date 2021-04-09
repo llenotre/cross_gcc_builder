96344816676e2e4d08942288ac36778dcb87b1f6
llenotre <llenotre@student.42.fr>
2021-04-09 13:19:11 +0200

    Uncommented binutils build commands and updated README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains scripts to build the GCC cross compiler for the i686 platform.
 
 - `download.sh`: Downloads the compiler's sources
-- `build_compiler.sh`: Builds the compiler from sources
+- `build_compiler.sh`: Builds the compiler from sources and installs it on the current system
 - `clean.sh`: Cleans the directory
 
 
--- build_compiler.sh
+++ build_compiler.sh
@@ -2,12 +2,12 @@
 
 export TARGET=i686-elf
 
-#mkdir binutils_build/
-#cd binutils_build
-#../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
-#make
-#make install
-#cd ..
+mkdir binutils_build/
+cd binutils_build
+../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
+make
+make install
+cd ..
 
 mkdir gcc_build/
 cd gcc_build
