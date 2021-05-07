662d7bd421d68803898c6d84677323116c20196b
llenotre <llenotre@student.42.fr>
2021-05-07 22:39:56 +0200

    Fix

--- build_compiler.sh
+++ build_compiler.sh
@@ -6,7 +6,11 @@ mkdir binutils_build/
 cd binutils_build
 ../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
 make
-make install
+sudo make install
+cd ..
+
+cd gcc-10.2.0/
+./contrib/download_prerequisites
 cd ..
 
 mkdir gcc_build/
@@ -14,6 +18,6 @@ cd gcc_build
 ../gcc-10.2.0/configure --target=$TARGET --prefix=/usr --disable-nls --enable-languages=c --without-headers
 make all-gcc
 make all-target-libgcc
-make install-gcc
-make install-target-libgcc
+sudo make install-gcc
+sudo make install-target-libgcc
 cd ..
