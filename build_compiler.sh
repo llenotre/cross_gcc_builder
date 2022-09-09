c38b66a708c5c84cba46ae6dc92c400bf3ceebda
llenotre <llenotre@student.42.fr>
2022-09-09 19:51:38 +0200

    Added multicore compilation

--- build_compiler.sh
+++ build_compiler.sh
@@ -5,7 +5,7 @@ export TARGET=i686-elf
 mkdir binutils_build/
 cd binutils_build
 ../binutils-2.36/configure --target=$TARGET --prefix=/usr --with-sysroot --disable-nls --disable-werror
-make
+make -j$(nproc)
 sudo make install
 cd ..
 
@@ -16,8 +16,8 @@ cd ..
 mkdir gcc_build/
 cd gcc_build
 ../gcc-10.2.0/configure --target=$TARGET --prefix=/usr --disable-nls --enable-languages=c --without-headers
-make all-gcc
-make all-target-libgcc
+make all-gcc -j$(nproc)
+make all-target-libgcc -j$(nproc)
 sudo make install-gcc
 sudo make install-target-libgcc
 cd ..
